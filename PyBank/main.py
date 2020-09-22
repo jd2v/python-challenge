5e5a18af52c051781cb2b46e9d34782480e17d82
Justin DeVries <justin.debeesknees@gmail.com>
2020-09-22 16:22:32 -0500

    adds lists to PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,3 +2,11 @@ import csv
 import os
 # Path to resource data
 bank_csv = os.path.join("..","Resources","budget_data.csv")
+#Lists to store data
+dates = []
+profits = []
+losses = []
+# Open the data
+with open(bank_csv) as csvfile:
+    bankreader = csv.reader(csvfile, delimiter=",")
+

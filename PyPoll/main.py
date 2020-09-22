590d35f5622540e668abc18cc6be7c94ccbb1536
Justin DeVries <justin.debeesknees@gmail.com>
2020-09-22 18:40:36 -0500

    adds vote counter

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,14 @@
 import csv
 import os
 # Path to resource data
-bank_csv = os.path.join("..","Resources","election_data.csv")
+election_csv = os.path.join("..","Resources","election_data.csv")
+# variables for loops
+counter = 0
+# Open the data
+with open(election_csv) as csvfile:
+    election_reader = csv.reader(csvfile, delimiter=",")
+    election_header = next(election_reader)
+    for row in election_reader:
+        # increase counter to find vote numbers
+        counter = counter + 1
+        

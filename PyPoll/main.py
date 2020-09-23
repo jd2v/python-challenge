080c5e60748ff7475fda53f6700194e063317cd7
Justin DeVries <justin.debeesknees@gmail.com>
2020-09-22 21:48:46 -0500

    Adds unique cand_name grabber

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,14 +1,20 @@
 import csv
 import os
 # Path to resource data
-election_csv = os.path.join("..","Resources","election_data.csv")
+election_csv = os.path.join("Resources","election_data.csv")
 # variables for loops
-counter = 0
+total_vote_counter = 0
+#lists for loops
+cand_votes = []
+cand_names = []
 # Open the data
 with open(election_csv) as csvfile:
     election_reader = csv.reader(csvfile, delimiter=",")
     election_header = next(election_reader)
     for row in election_reader:
         # increase counter to find vote numbers
-        counter = counter + 1
-        
+        total_vote_counter = total_vote_counter + 1
+        #Add name to cand_name if not already present in list
+        if row[2] not in cand_names:
+            cand_names.append(row[2])
+print(cand_names)

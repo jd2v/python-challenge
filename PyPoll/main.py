d531c9234992498707d2df447dc823557ec9eb32
Justin DeVries <justin.debeesknees@gmail.com>
2020-09-22 22:07:04 -0500

    adds cand vote counters

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,8 +4,11 @@ import os
 election_csv = os.path.join("Resources","election_data.csv")
 # variables for loops
 total_vote_counter = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
 #lists for loops
-cand_votes = []
 cand_names = []
 # Open the data
 with open(election_csv) as csvfile:
@@ -17,4 +20,14 @@ with open(election_csv) as csvfile:
         #Add name to cand_name if not already present in list
         if row[2] not in cand_names:
             cand_names.append(row[2])
-print(cand_names)
+            
+        #count votes for cands
+        elif row[2] == "Khan":
+            khan_votes += 1
+        elif row[2] == "Li":
+            li_votes += 1
+        elif row[2] == "Correy":
+            correy_votes += 1
+        elif row[2] == "O'Tooley":
+            otooley_votes += 1
+print(str(otooley_votes))
